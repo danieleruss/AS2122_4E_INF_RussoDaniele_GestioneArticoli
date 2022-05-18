bbdc7c0fd758562a4c63b6ffa7fb0568fad5b3a9
PC03 <danyrusso0522@gmail.com>
2022-05-18 10:12:28 +0200

    main e articolo

---
+++ Nome AS2122_4E_INF_RussoDaniele_GestioneArticoli/FrmArticolo.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
+{
+    public partial class frmArticolo : Form
+    {
+        DialogResult status = DialogResult.Cancel;
+
+        // TODO: (5) aggiungere attributi privati dei dati inseriti nella frmArticoli
+        // ...
+        private string descrizione;
+        private string unitaMisura;
+        private double prezzo;
+
+
+        public DialogResult Status { get { return status; } }
+
+        // TODO: (6) aggiungere property di sola lettura dei dati inseriti nella frmArticoli per l'utilizzo in frmMain
+        // ...
+
+        public string Descrizione
+        {
+            get { return descrizione; }
+        }
+
+        public string UnitaMisura
+        {
+            get { return unitaMisura; }
+        }
+
+        public double Prezzo
+        {
+            get { return prezzo; }
+        }
+
+        public frmArticolo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalva_Click(object sender, EventArgs e)
+        {
+            // TODO: (7) passaggio all' attributo/property dei dati inseriti nella frmArticoli con controllo di valorizzazione del dato
+            // ... descrizione, unitaMisura, prezzo
+            if (txtPrezzo.text != "" && txtDescrizione.text != "" & cmbUnitaMisura.text != "")
+            {
+                descrizione = txtDescrizione.text;
+                unitaMisura = cmbUnitaMisura;
+                prezzo = txtPrezzo;
+            }
+            else
+            {
+                Message.Box("Inserisci tutti i dati");
+            }
+            status = DialogResult.OK;
+            Close();
+        }
+
+        private void btnAnnulla_Click(object sender, EventArgs e)
+        {
+            status = DialogResult.Cancel;
+            Close();
+        }
+    }
+}
---
+++ Nome AS2122_4E_INF_RussoDaniele_GestioneArticoli/frmMain.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
+{
+    public partial class frmMain : Form
+    {
+        List<Articolo> articoli;
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+            // TODO: (4) creazione lista articoli
+            // ...
+            List<Articolo> articoli = new List<Articolo>();
+
+        }
+
+        private void btnAggiungiArticolo_Click(object sender, EventArgs e)
+        {
+            frmArticolo f = new frmArticolo();
+
+            f.ShowDialog();
+
+            if (f.Status == DialogResult.OK)
+            {
+                // aggiungi l'articolo all'elenco di articoli
+                // TODO: (1) aggiungere l'articolo creato dai dati di frmArticoli nella lista articoli
+                // ...
+                Articolo a = new Articolo();
+                articoli.add(a);
+                lblArticoliInseriti.Text = $"Articoli ({articoli.Count})";
+            }
+        }
+
+
+        private void btnVisualizza_Click(object sender, EventArgs e)
+        {
+            lstElenco.Items.Clear();
+            switch (cmbVisualizza.Text)
+            {
+                case "Visualizza articoli":
+                    // TODO: (2) aggiungere visualizzazione articoli inseriti nella listbox
+                    foreach (Articolo a in articoli)
+                    {
+                        lstElenco.Items.Add(a.Visualizzati());
+                    }
+
+                    break;
+            }
+        }
+
+        private void lstElenco_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string curItem = lstElenco.SelectedItem.ToString();
+
+            // Find the string in ListBox.
+            int index = lstElenco.FindString(curItem);
+
+            // TODO: (3) aggiungere visualizzazione dettaglio articolo nelle label
+            lblDescrizione.Text = articoli[index].Descrizione;
+            lblArticoliInseriti.Text = articoli[index].ArticoliInseriti;
+            lblPrezzo.Text = articoli[index].Prezzo;
+            lblUnitaMisura.Text = articoli[index].UnitaMisura;
+
+        }
+    }
+}
