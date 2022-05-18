586d80530e53db06883f9ec27e73fd77829d34cd
PC03 <PC03@INFO5-5374>
2022-05-18 09:10:49 +0200

    Aggiungere i file di progetto.

---
+++ Nome AS2122_4E_INF_RussoDaniele_GestioneArticoli/Form1.cs
@@ -0,0 +1,10 @@
+namespace Nome_AS2122_4E_INF_RussoDaniele_GestioneArticoli
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Nome AS2122_4E_INF_RussoDaniele_GestioneArticoli/Program.cs
@@ -0,0 +1,17 @@
+namespace Nome_AS2122_4E_INF_RussoDaniele_GestioneArticoli
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
